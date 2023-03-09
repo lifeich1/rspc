846754742d25ef5d2d968d114cc3a78973913eb1
lifeich1 <lifeich0@gmail.com>
2023-03-10 06:11:58 +0800

    :sparkles: ccalgo: postorder_travel opt feature

--- ccalgo/network/postorder_travel.cc
+++ ccalgo/network/postorder_travel.cc
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#define POSTORDER_TRAVEL__S_TREEM
+#define POSTORDER_TRAVEL__S_PAPA
+
 #include "sparse_net.hh"
 #include "network01trait.hh"
 #include "postorder_travel.hh"
@@ -18,6 +21,7 @@ int main() {
     typedef PT<9> PT9;
     typedef std::vector<std::size_t> Nv;
     {
+        std::vector<std::size_t> pa;
         Nw9 nw, tnw;
         nw.link(0, 1);
         nw.link(1, 2);
@@ -25,13 +29,15 @@ int main() {
         nw.link(3, 4);
         nw.link(4, 5);
 
-        PT9 pt{&nw};
-        pt.tree_memory(&tnw);
+        PT9 pt{&nw, &tnw, &pa};
         Nv v;
         pt(0, std::back_inserter(v));
         std::copy(v.begin(), v.end(), std::ostream_iterator<std::size_t>(std::cout, " "));
         std::cout << std::endl;
+        std::copy(pa .begin(), pa .end() , std::ostream_iterator<std::size_t>(std::cout, " "));
+        std::cout << std::endl;
         ASSERT_EQ(v, Nv({5, 4, 3, 2, 1, 0}));
+        ASSERT_EQ(pa, Nv({0, 0, 1, 2, 3, 4}));
         ASSERT_EQ(tnw.edges(5).begin(), tnw.edges(5).end());
         v.clear();
         std::transform(tnw.edges(0).begin(), tnw.edges(0).end(),
@@ -41,19 +47,22 @@ int main() {
     }
     {
         Nw9 nw, tnw;
+        Nv pa;
         nw.link(1, 5);
         nw.link(1, 6);
         nw.link(1, 2);
         nw.link(2, 3);
         nw.link(2, 4);
 
-        PT9 pt{&nw};
-        pt.tree_memory(&tnw);
+        PT9 pt{&nw, &tnw, &pa};
         Nv v;
         pt(1, std::back_inserter(v));
         std::copy(v.begin(), v.end(), std::ostream_iterator<std::size_t>(std::cout, " "));
         std::cout << std::endl;
         ASSERT_EQ(v, Nv({5, 6, 3, 4, 2, 1}));
+        ASSERT_EQ(pa, Nv({0, 1, 1, 2, 2, 1, 1}));
+        std::copy(pa .begin(), pa .end() , std::ostream_iterator<std::size_t>(std::cout, " "));
+        std::cout << std::endl;
         ASSERT_EQ(tnw.edges(5).begin(), tnw.edges(5).end());
         v.clear();
         std::transform(tnw.edges(2).begin(), tnw.edges(2).end(),
--- ccalgo/network/postorder_travel.hh
+++ ccalgo/network/postorder_travel.hh
@@ -1,23 +1,51 @@
 #pragma once
 #include <array>
 
+#if 0
+#define POSTORDER_TRAVEL__S_TREEM
+#define POSTORDER_TRAVEL__S_PAPA
+#endif
+
 namespace A {
 template <std::size_t N, class Net>
 class PostorderTravel {
 public:
     typedef std::size_t vertex_t;
 
-    explicit PostorderTravel(Net * net): net{net}, tree{nullptr}, cur{0} {
+    explicit PostorderTravel(Net * net
+#ifdef POSTORDER_TRAVEL__S_TREEM
+            , Net * tree
+#endif
+#ifdef POSTORDER_TRAVEL__S_PAPA
+            , std::vector<std::size_t> * papa
+#endif
+            ): net{net}
+#ifdef POSTORDER_TRAVEL__S_TREEM
+            , tree{tree}
+#endif
+#ifdef POSTORDER_TRAVEL__S_PAPA
+            , papa{papa}
+#endif
+    {
         std::fill(flag.begin(), flag.end(), cur);
     }
 
-    void tree_memory(Net * net) { tree = net; }
-
+#ifdef POSTORDER_TRAVEL__S_PAPA
+# define SET_PA(A, B) set_pa(A, B)
+#else
+# define SET_PA(A, B)
+#endif
+#ifdef POSTORDER_TRAVEL__S_TREEM
+# define TR_AR(A, B) tree->arrow(A, B)
+#else
+# define TR_AR(A, B)
+#endif
     template <class Index, class OutputIt>
     void operator() (Index src, OutputIt d_first) {
         stk[0] = src;
         int p = 0;
         flag[src] = ++cur;
+        SET_PA(src, src);
         while (p >= 0) {
             const auto lp = p;
             auto x = stk[p];
@@ -25,7 +53,8 @@ public:
                 if (cur != flag[v]) {
                     flag[v] = cur;
                     stk[++p] = v;
-                    if (tree) tree->arrow(x, v);
+                    SET_PA(v, x);
+                    TR_AR(x, v);
                 }
             }
             if (lp == p) {
@@ -34,11 +63,25 @@ public:
             }
         }
     }
+#undef SET_PA
+#undef TR_AR
 private:
+#ifdef POSTORDER_TRAVEL__S_PAPA
+    inline void set_pa(vertex_t v, vertex_t pa) {
+        if (papa->size() <= v) papa->resize(v + 1);
+        papa->at(v) = pa;
+    }
+#endif
+
     Net * net;
+#ifdef POSTORDER_TRAVEL__S_TREEM
     Net * tree;
+#endif
+#ifdef POSTORDER_TRAVEL__S_PAPA
+    std::vector<vertex_t> * papa;
+#endif
     std::array<int, N> flag;
     std::array<vertex_t, N> stk;
-    int cur;
+    int cur = 0;
 };
 }
