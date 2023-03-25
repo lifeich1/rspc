9f73d60feede47bef9aa82c238162fe365d2ca18
lifeich1 <lifeich0@gmail.com>
2023-03-25 16:48:17 +0800

    :sparkles: ccalgo: arena.mk add genchk feature

--- ccalgo/arena.mk
+++ ccalgo/arena.mk
@@ -13,3 +13,28 @@ E := $(basename $(F))
 
 $(E): $(F)
 	$(CC) $(CCFLAGS) -o $@ $<
+
+
+.PHONY: genchk
+genchk: sf := ./t0.sh
+genchk: pf := ./t1.py
+genchk: s := echo >> $(sf)
+genchk: p := echo >> $(pf)
+genchk:
+	test ! -f $(sf) && test ! -f $(pf)
+	$(p) 'from random import *'
+	$(p) 'n = randint(2, 9)'
+	$(s) '#!/bin/bash'
+	$(s) 'touch keeptest'
+	$(s) 'g++ main.cc -o 1main'
+	$(s) 'g++ ans.cc -o 1ans'
+	$(s) 'while [ -f keeptest ]; do'
+	$(s) '	python3 ./t1.py > 1.in'
+	$(s) '	./1ans < 1.in > 1.ans'
+	$(s) '	./1main < 1.in > 1.out'
+	$(s) '	diff 1.out 1.ans || break'
+	$(s) '	echo -n "OK "'
+	$(s) 'done'
+	$(s) 'echo'
+	$(s) 'if [ -f keeptest ]; then echo WA 1.in; head 1.in; fi'
+	chmod +x $(sf)
