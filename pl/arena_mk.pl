5f47361b00f40345b202ca620ceb909e3f848a1c
lifeich1 <lifeich0@gmail.com>
2023-07-22 01:39:01 +0800

    :construction: arena_mk: group build code, adding test cmd

--- pl/arena_mk.pl
+++ pl/arena_mk.pl
@@ -8,28 +8,43 @@ use open qw/:std :utf8/;
 my $src = shift;
 my $cmd = shift;
 
-chomp( my $btim = qx(date +%D-%Hh-%Mm-%Ss) );
 my $bin = $src;
-$bin =~ s/(.+)\.(?:cc|cpp)/$1/;
+if ( $src =~ /(.+)\.(?:cc|cpp)/ ) {
+  $bin = $1;
+  &build( $src, $bin );
+}
 
-# TODO test modify time for skip
+sub reg_test {
+  my ($bin) = @_;
+}
 
-# TODO use bear only if available
+sub build {
+  my ( $src, $bin ) = @_;
+  return if ( -f $bin && -r _ && -w _ && -x _ && ( ( -M _ ) < ( -M $src ) ) );
 
-my @ex = (
-  qw(
-    bear -- g++ -std=c++17 -g -Wall -DRSPC_TRACE_HINT=1
-    -I/usr/include/x86_64-linux-gnu/c++/11
-    -I/usr/lib/gcc/x86_64-linux-gnu/11/include
-    -I/usr/include/x86_64-linux-gnu
-    -I/usr/include/c++/11
-    -I/usr/include/c++/11/backward
-  ),
-  qq(-DRSPC_TRACE_BTIME="${btim}"),
-  $src, '-o', $bin,
-);
-say join ' ', @ex;
+  chomp( my $btim = qx(date +%D-%Hh-%Mm-%Ss) );
 
-system(@ex) == 0 or die('make failed');
+  my @bear_prefix = ();
+  if ( 0 == system(qw{which bear > /dev/null}) ) {
+    @bear_prefix = qw/bear --/;
+  }
+
+  my @ex = (
+    @bear_prefix,
+    qw(
+      g++ -std=c++17 -g -Wall -DRSPC_TRACE_HINT=1
+      -I/usr/include/x86_64-linux-gnu/c++/11
+      -I/usr/lib/gcc/x86_64-linux-gnu/11/include
+      -I/usr/include/x86_64-linux-gnu
+      -I/usr/include/c++/11
+      -I/usr/include/c++/11/backward
+    ),
+    qq(-DRSPC_TRACE_BTIME="${btim}"),
+    $src, '-o', $bin,
+  );
+  say join ' ', @ex;
+
+  system(@ex) == 0 or die('make failed');
+}
 
 # vim: ts=2 sw=2
