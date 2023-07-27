449697bccd539fb87b9302f28a35fb43eefc4c49
lifeich1 <lifeich0@gmail.com>
2023-07-28 00:53:50 +0800

    :art: ccalgo: fmt miller_rabin

--- ccalgo/numeric/miller_rabin.cc
+++ ccalgo/numeric/miller_rabin.cc
@@ -4,16 +4,16 @@
 typedef A::MillerRabinPrimeTest<> Ptest;
 
 int main() {
-    Ptest ptest;
-    ASSERT_EQ(ptest(99999999), false);
-    ASSERT_EQ(ptest(100000007), true);
-    ASSERT_EQ(ptest(998244353), true);
-    ASSERT_EQ(ptest(1073741789), true);
+  Ptest ptest;
+  ASSERT_EQ(ptest(99999999), false);
+  ASSERT_EQ(ptest(100000007), true);
+  ASSERT_EQ(ptest(998244353), true);
+  ASSERT_EQ(ptest(1073741789), true);
 
-    for (auto k = (1ll << 19); ; --k)
-        if (ptest(k)) {
-            std::cout << k << std::endl;
-            break;
-        }
-    return 0;
+  for (auto k = (1ll << 29);; --k)
+    if (ptest(k)) {
+      std::cout << k << std::endl;
+      break;
+    }
+  return 0;
 }
