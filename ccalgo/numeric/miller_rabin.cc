589d1f04567411f129f2b9481d0308b17473d4aa
lifeich1 <lifeich0@gmail.com>
2023-08-11 00:15:54 +0800

    :wheelchair: ccalgo/miller_rabin: easy access

--- ccalgo/numeric/miller_rabin.cc
+++ ccalgo/numeric/miller_rabin.cc
@@ -1,10 +1,8 @@
 #include "miller_rabin.hh"
 #include "common/assert.hh"
 
-typedef A::MillerRabinPrimeTest<> Ptest;
-
 int main() {
-  Ptest ptest;
+  auto ptest = fast_prime_tester();
   ASSERT_EQ(ptest(99999999), false);
   ASSERT_EQ(ptest(100000007), true);
   ASSERT_EQ(ptest(998244353), true);
--- ccalgo/numeric/miller_rabin.hh
+++ ccalgo/numeric/miller_rabin.hh
@@ -1,17 +1,21 @@
 #pragma once
 
-#include <vector>
 #include "calc_in_mod.hh"
+#include <vector>
 
 namespace A {
-template <class T = int64_t>
-struct MillerRabinPrimeTest {
-    std::vector<T> test = {2,3,5,7,11,13,17,19,23,29};
+template <class T = int64_t> struct MillerRabinPrimeTest {
+  std::vector<T> test = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
 
-    bool operator() (T const p) const {
-        for (auto y : test)
-            if (1 != qpow_in_mod(y, p - 1, p)) return false;
-        return true;
-    }
+  bool operator()(T const p) const {
+    for (auto y : test)
+      if (1 != qpow_in_mod(y, p - 1, p))
+        return false;
+    return true;
+  }
 };
+} // namespace A
+
+template <class T = int64_t> A::MillerRabinPrimeTest<T> fast_prime_tester() {
+  return {};
 }
