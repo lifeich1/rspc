8538c97cd71a8ae644f23ec5c2eb17539a451bb1
lifeich1 <lifeich0@gmail.com>
2023-08-27 18:29:58 +0800

    :bug: calc_in_mod: fix default insert value

--- ccalgo/numeric/calc_in_mod.hh
+++ ccalgo/numeric/calc_in_mod.hh
@@ -4,49 +4,58 @@
 
 namespace A {
 
-template <class T>
-inline constexpr T qpow_in_mod(T base, T ind, T m) {
-    T res = 1;
-    for (T bi = base; ind > 0; ind >>= 1, bi = (bi * bi) % m)
-        if (ind & 1) res = (res * bi) % m;
-    return res;
+template <class T> inline constexpr T qpow_in_mod(T base, T ind, T m) {
+  T res = 1;
+  for (T bi = base; ind > 0; ind >>= 1, bi = (bi * bi) % m)
+    if (ind & 1)
+      res = (res * bi) % m;
+  return res;
 }
 
-template <int64_t M, class T = int64_t>
-class CalcInMod {
+template <int64_t M, class T = int64_t> class CalcInMod {
 public:
-    typedef T data_t;
+  typedef T data_t;
 
-    T v;
+  T v;
 
-    CalcInMod() {}
-    CalcInMod(int v): v{static_cast<T>(v)} {}
-    CalcInMod(T v): v{v} {}
+  CalcInMod() : v{0} {}
+  CalcInMod(int v) : v{static_cast<T>(v)} {}
+  CalcInMod(T v) : v{v} {}
 
-    int as_i() { return static_cast<int>(v); }
+  int as_i() { return static_cast<int>(v); }
 
-    using type = CalcInMod<M, T>;
+  using type = CalcInMod<M, T>;
 
-    inline static T add(T a, T b) { return (a + b) % M; }
-    inline static T sub(T a, T b) { return (a - b + M) % M; }
-    inline static T mul(T a, T b) { return (a * b) % M; }
-    inline static T qpow(T base, T ind) {
-        T res = 1;
-        for (T bi = base; ind > 0; ind >>= 1, bi = mul(bi, bi))
-            if (ind & 1) res = mul(res, bi);
-        return res;
-    }
+  inline static T add(T a, T b) { return (a + b) % M; }
+  inline static T sub(T a, T b) { return (a - b + M) % M; }
+  inline static T mul(T a, T b) { return (a * b) % M; }
+  inline static T qpow(T base, T ind) {
+    T res = 1;
+    for (T bi = base; ind > 0; ind >>= 1, bi = mul(bi, bi))
+      if (ind & 1)
+        res = mul(res, bi);
+    return res;
+  }
 
-    inline type operator+ (type const & a) const { return {(a.v + v) % M}; }
-    inline type operator- (type const & a) const { return {(v - a.v + M) % M}; }
-    inline type operator* (type const & a) const { return {(a.v * v) % M}; }
-    inline type & operator+= (type const & a) { *this = *this + a; return *this; }
-    inline type & operator-= (type const & a) { *this = *this - a; return *this; }
-    inline type & operator*= (type const & a) { *this = *this * a; return *this; }
-    inline type mul_inv() const { return {qpow(M - 2)}; }
-    inline type qpow(T ind) const { return {qpow(v, ind)}; }
+  inline type operator+(type const &a) const { return {(a.v + v) % M}; }
+  inline type operator-(type const &a) const { return {(v - a.v + M) % M}; }
+  inline type operator*(type const &a) const { return {(a.v * v) % M}; }
+  inline type &operator+=(type const &a) {
+    *this = *this + a;
+    return *this;
+  }
+  inline type &operator-=(type const &a) {
+    *this = *this - a;
+    return *this;
+  }
+  inline type &operator*=(type const &a) {
+    *this = *this * a;
+    return *this;
+  }
+  inline type mul_inv() const { return {qpow(M - 2)}; }
+  inline type qpow(T ind) const { return {qpow(v, ind)}; }
 
-    inline bool operator== (type const & a) const { return v == a.v; }
-    inline bool operator< (type const & a) const { return v < a.v; }
+  inline bool operator==(type const &a) const { return v == a.v; }
+  inline bool operator<(type const &a) const { return v < a.v; }
 };
-}
+} // namespace A
